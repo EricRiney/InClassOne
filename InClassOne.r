d356a284b6ee03d9e666e4b15e70c18feffc2961
EricRiney <fails.to.install@gmail.com>
2016-10-29 17:43:25 -0700

    r added

---
+++ InClassOne.r
@@ -0,0 +1,8 @@
+# Eric Riner
+# InClassOne
+
+
+trailBlazers = c(90,95,89,71,73,96,87,95, 107,89,96,80,97,95, 102,97,93, 101,82,83,74,91,83,98,95, 111,99, 120,93,84)
+summary(trailBlazers)
+   Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+  71.00   84.75   94.00   92.20   97.00  120.00 
